80479cb62db4642b3f721cf1674de532c6c4a392
y2Dev <dub-y@tuta.io>
2022-04-20 08:46:18 -0400

    ajout helper dans lien contact du navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="w-24 flex flex-col items-center bg-gray-900 py-4">
-    <div class="text-lg font-semibold text-white">Le Festin</div>
+    <div class="text-lg font-semibold text-white">Ti Doucè</div>
     <!-- nav content -->
     <ul class="mt-2 text-gray-300 font-semibold">
       <li class="mt-3 t">
-        <a href="#/" class="flex flex-col items-center text-sm capitalize">
+        <a href="/" class="flex flex-col items-center text-sm capitalize">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
           </svg>
@@ -12,9 +12,9 @@
         </a>
       </li>
       <li class="mt-3 t">
-        <a href="/contactez-nous" class="flex flex-col items-center text-sm capitalize">
+        <a href="{{ route("contact") }}" class="flex flex-col items-center text-sm capitalize">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
           </svg>
           <span>Contact</span>
         </a>
